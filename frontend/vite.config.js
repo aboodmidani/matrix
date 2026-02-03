4175528a890178b06327e28a14907bf3d4d4d3c3
aboodmidani <55650856+aboodmidani@users.noreply.github.com>
2026-02-03 09:55:55 +0100

    fixed cors and docker

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,6 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   define: {
-    'import.meta.env.CORS_ORIGINS': JSON.stringify(process.env.CORS_ORIGINS || 'http://localhost:8001')
+    'import.meta.env.CORS_ORIGINS': JSON.stringify(process.env.CORS_ORIGINS)
   }
 })
